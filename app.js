3fbd7902e69ace4d9625a8f0b9f5cdbd6a3bb077
Kawish Zia <kawishzia2826@gmail.com>
2024-11-01 05:28:52 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,72 @@
+
+//------------------------------------------------------------------------------- Assignment 06---------------------------------------------------------------------------------
+           //----------------------------------------------------------------------Task No 1----------------------------------------------------------------------
+
+
+// document.write("<h1>Result<h1/>");
+// document.write("The value of a is: 10");
+// num = 10;
+// num++;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 11;
+// num++;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 12;
+// num--;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 11;
+// --num;
+// document.write("Now the value of is :", num);
+
+//----------------------------------------------------------------------Task No 2----------------------------------------------------------------------
+
+
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+
+//             1 - 0 + 2 + 0 = 3
+
+// console.log(result);
+
+
+
+//==========================================================Assignment No   (10)======================================================================================
+//----------------------------------------------------------Task No (01) ---------------------------------------------------------------------------------------------
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+// var studentNames = [];
+// console.log(studentNames = ["Asdsa"])
+
+// 3. Declare and initialize a strings array.
+// var studentNames = ["kawihs","jamal","habib"];
+
+//4. Declare and initialize a numbers array.
+// var studentNames = [1,2,3,4];
+// 5. Declare and initialize a boolean array.
+// var studentNames = [true,false];
+// // 6. Declare and initialize a mixed array.
+// var studentNames = [true,false,1,2,"a","b"];
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+// var courses  = ["SSC", "HSC", "BCS",
+//         "BS", "BCOM", "MS", "M", "Phil", "PhD"]
+//         8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:\
+// var  students = ["Kawish Zia","Jamal","Kamal"]
+// var scorStudent = [480,320,430];
+// console.log(`${students[0]} ${scorStudent[0]} score hai ${(scorStudent[0] / 500)*100} `);
+// console.log(`${students[1]} ${scorStudent[1]} score hai ${(scorStudent[1] / 500)*100} `);
+// console.log(`${students[2]} ${scorStudent[2]} score hai ${(scorStudent[2] / 500)*100} `);
